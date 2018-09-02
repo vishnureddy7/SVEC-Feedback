d4e5404d95c321ab7203b2145bf2d9e43a8542f1
guru vishnuvardhan reddy desireddy <vishnuvardhan12137@gmail.com>
2018-09-02 12:10:38 +0530

    vishnu

---
+++ app/src/main/java/com/example/supriyak/svecfeedback/StudentFeedback.java
@@ -0,0 +1,13 @@
+package com.example.supriyak.svecfeedback;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StudentFeedback extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_feedback);
+    }
+}
