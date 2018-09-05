5f4f2d3d33eceb0febe91e592e9d39993f87a845
guru vishnuvardhan reddy desireddy <vishnuvardhan12137@gmail.com>
2018-09-05 19:18:43 +0530

    integrated

--- app/src/main/java/com/example/supriyak/svecfeedback/AlumniFeedbackForm.java
+++ app/src/main/java/com/example/supriyak/svecfeedback/AlumniFeedbackForm.java
@@ -3,11 +3,11 @@ package com.example.supriyak.svecfeedback;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class StudentFeedback extends AppCompatActivity {
+public class AlumniFeedbackForm extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_student_feedback);
+        setContentView(R.layout.activity_alumni_feedback_form);
     }
 }
---
+++ app/src/main/java/com/example/supriyak/svecfeedback/RegistrationThankyou.java
@@ -0,0 +1,13 @@
+package com.example.supriyak.svecfeedback;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistrationThankyou extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_thankyou);
+    }
+}
